f1a5a30ed4b74569cad148ef7af617ac082b4821
Chen Su <ghosind@gmail.com>
2022-06-15 17:29:26 +0800

    style: add readonly modifier and blank lines between properties

--- src/viewer-request/event.d.ts
+++ src/viewer-request/event.d.ts
@@ -8,7 +8,8 @@ interface CFFunctionEvent {
    * @example '1.0'
    * @see https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/functions-event-structure.html#functions-event-structure-version
    */
-  version: string;
+  readonly version: string;
+
   /**
    * The context object contains contextual information about the event.
    * @example
@@ -19,6 +20,7 @@ interface CFFunctionEvent {
    *   requestId: 'EXAMPLEntjQpEXAMPLE_SG5Z-EXAMPLEPmPfEXAMPLEu3EqEXAMPLE==',
    * }
    */
+
   context: CFFunctionContext;
   /**
    * The viewer object contains
@@ -28,6 +30,7 @@ interface CFFunctionEvent {
    * }
    */
   viewer: CFFunctionViewer;
+
   /**
    * The request object contains a representation of a viewer-to-CloudFront HTTP request. In the
    * event object that’s passed to your function, the request object represents the actual request
@@ -36,6 +39,7 @@ interface CFFunctionEvent {
    * If your function code returns a request object to CloudFront, it must use this same structure.
    */
   request: CFFunctionRequest;
+
   /**
    * The response object contains a representation of a CloudFront-to-viewer HTTP response. In the
    * event object that’s passed to your function, the response object represents CloudFront’s actual
@@ -55,20 +59,23 @@ interface CFFunctionContext {
    * The CloudFront domain name of the distribution that’s associated with the event.
    * @example 'd111111abcdef8.cloudfront.net'
    */
-  distributionDomainName: string;
+  readonly distributionDomainName: string;
+
   /**
    * The ID of the distribution that’s associated with the event.
    * @example 'EDFDVBD6EXAMPLE'
    */
-  distributionId: string;
+  readonly distributionId: string;
+
   /**
    * The event type, either `viewer-request` or `viewer-response`.
    */
-  eventType: CFFunctionEventType;
+  readonly eventType: CFFunctionEventType;
+
   /**
    * A string that uniquely identifies a CloudFront request (and its associated response).
    */
-  requestId: string;
+  readonly requestId: string;
 }
 
 /**
@@ -92,7 +99,8 @@ interface CFFunctionRequest {
   /**
    * The HTTP method of the request. It is a read-only field in the request object.
    */
-  method: string;
+  readonly method: string;
+
   /**
    * The relative path of the requested object. If your function modifies the uri value, note the
    * following:
@@ -102,11 +110,13 @@ interface CFFunctionRequest {
    *   or the origin that an origin request is sent to.
    */
   uri: string;
+
   /**
    * An object that represents the query string in the request. If the request doesn’t include a
    * query string, the request object still includes an empty querystring object.
    */
   querystring: CFFunctionData;
+
   /**
    * An object that represents the HTTP headers in the request. If the request contains any
    * `Cookie` headers, those headers are not part of the headers object. Cookies are
@@ -120,6 +130,7 @@ interface CFFunctionRequest {
    * `example-header-name`, CloudFront converts this to `Example-Header-Name` in the HTTP request.
    */
   headers: CFFunctionData;
+
   /**
    * An object that represents the cookies in the request (`Cookie` headers).
    */
@@ -135,17 +146,20 @@ interface CFFunctionResponse {
    * The HTTP status code of the response. This value is an integer, not a string.
    */
   statusCode: number;
+
   /**
    * The HTTP status description of the response. If your function code generates a response, this
    * field is optional.
    */
   statusDescription: string;
+
   /**
    * An object that represents the HTTP headers in the response. If the response contains any
    * `Set-Cookie` headers, those headers are not part of the headers object. Cookies are
    * represented separately in the cookies object.
    */
   headers: CFFunctionData;
+
   /**
    * An object that represents the cookies in the response (`Set-Cookie` headers).
    */
@@ -170,11 +184,13 @@ interface CFFunctionDataValue {
    * It contains the value of the query string, header, or cookie.
    */
   value: string;
+
   /**
    * It contains an array with the values of each of the duplicate query strings, headers, or
    * cookies.
    */
   multiValue?: CFFunctionDataValue[];
+
   /**
    * Property attributes for cookie in the response.
    */
@@ -185,6 +201,13 @@ interface CFFunctionDataValue {
  * CloudFront Functions event types.
  */
 enum CFFunctionEventType {
+  /**
+   * Event for Viewer request.
+   */
   ViewerRequest = 'viewer-request',
+
+  /**
+   * Event for Viewer response.
+   */
   ViewerResponse = 'viewer-response',
 }
